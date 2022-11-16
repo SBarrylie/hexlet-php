9ebd7feb065e7397dffd3e40965532f33c927be6
SBarrylie <xcedorg@yandex.ru>
2022-11-16 11:34:17 +0000

    Вывод index.php зависит от src/Runner.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
-$a = "Hello, ";
-$b = 'Hexlet!';
-echo $a . $b;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается напрямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
+
